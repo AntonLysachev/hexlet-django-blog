af162aa2ce834e8138def336b9ce7008c331c1b9
AntonLysachev <anton@lysachev.ru>
2024-03-04 15:44:52 +0500

    add base.html

---
+++ hexlet_django_blog/article/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+TEAM = [
+    {'name': 'Yoda', 'position': 'CEO'},
+    {'name': 'Obi-Wan Kenobi', 'position': 'Senior Developer'},
+    {'name': 'Anakin Skywalker', 'position': 'Junior Developer'},
+    {'name': 'Jar Jar Binks', 'position': 'Trainee'},
+]
+
+def index(request):
+    return render(request, 'articles.html', context={
+        'body': TEAM,
+    })
