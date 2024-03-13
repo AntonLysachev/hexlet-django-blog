92cc35c7a37b99213b6ba02c9465798db783aad1
AntonLysachev <anton@lysachev.ru>
2024-03-13 10:52:52 +0500

    add article/index

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('', views.ArticlePageView.as_view(template_name='articles.html'), name='article'),
-    path('<slug:tag_id>/<int:article_id>/', views.TegArticlePageView.as_view(template_name='tag_article.html'), name='tag_article')
+    # path('', views.ArticlePageView.as_view(template_name='articles.html'), name='article'),
+    path('<slug:tag_id>/<int:article_id>/', views.TegArticlePageView.as_view(template_name='tag_article.html'), name='tag_article'),
+    path('', views.IndexView.as_view(), name='article')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,21 +1,37 @@
 from typing import Any
+from django.views import View
 from django.views.generic.base import TemplateView
+from hexlet_django_blog.article.models import Article
+from django.shortcuts import render
 
-TEAM = [
-    {'name': 'Yoda', 'position': 'CEO'},
-    {'name': 'Obi-Wan Kenobi', 'position': 'Senior Developer'},
-    {'name': 'Anakin Skywalker', 'position': 'Junior Developer'},
-    {'name': 'Jar Jar Binks', 'position': 'Trainee'},
-]
+# TEAM = [
+#     {'name': 'Yoda', 'position': 'CEO'},
+#     {'name': 'Obi-Wan Kenobi', 'position': 'Senior Developer'},
+#     {'name': 'Anakin Skywalker', 'position': 'Junior Developer'},
+#     {'name': 'Jar Jar Binks', 'position': 'Trainee'},
+# ]
 
 
-class ArticlePageView(TemplateView):
-    template_name = "articles.html"
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        return {'body': TEAM,}
+# class ArticlePageView(TemplateView):
+
+#     template_name = "articles.html"
+
+#     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+#         return {'body': TEAM,}
     
 
 class TegArticlePageView(TemplateView):
+
     template_name = "tags_article_id"
+
     def get_context_data(self, tag_id, article_id) -> dict[str, Any]:
         return {'tag_id': tag_id, 'article_id': article_id}
+
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
